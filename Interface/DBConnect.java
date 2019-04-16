0254356a8f0d3dc7c576fd28103d358ee474c8d7
istiakahmad <istiakahmad86@gmail.com>
2019-04-16 22:12:35 +0600

    first commit

---
+++ Interface/DBConnect.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interface;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author User
+ */
+public class DBConnect {
+    
+    public static Connection connect(){
+        Connection con = null;
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            con = (Connection)DriverManager.getConnection("jdbc:mysql://localhost:3306/library?", "root", "");
+            JOptionPane.showMessageDialog(null, "Connection Okay");
+
+        } catch (Exception e){
+            //JOptionPane.showMessageDialog(null, e);
+        }
+        return con;
+    }
+    
+}
